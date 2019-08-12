63610bcaca5bfb24e025b22f7c9508f76f2ea21f
surjit2003 <ssatapathy2003@gmail.com>
2019-08-12 16:38:21 +0530

    Adding first Java file

---
+++ ClassAndObject.java
@@ -0,0 +1,15 @@
+package PracticeJava;
+
+public class ClassAndObject {
+	int i;
+	String j;
+	public void display() {
+		System.out.println("Values are: "+ i  +j);
+	}
+
+	
+	public static void main (String args[]) {
+		System.out.println("Hello world");
+	}
+
+}
